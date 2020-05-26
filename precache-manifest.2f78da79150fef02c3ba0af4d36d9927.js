e5c737a6f686f9b75ac6f0bb79546e0b3fec0a2d
salferrari <sal@melodix.com>
2020-05-26 12:33:34 -0500

    Updates

---
+++ precache-manifest.2f78da79150fef02c3ba0af4d36d9927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc402f1b5ca4cd615ba53a016c22b6e0",
+    "url": "/m2/index.html"
+  },
+  {
+    "revision": "d75e63f05bafcba19c67",
+    "url": "/m2/static/css/main.71215815.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/m2/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/m2/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75e63f05bafcba19c67",
+    "url": "/m2/static/js/main.bbeadc25.chunk.js"
+  },
+  {
+    "revision": "a18bbfbed23119256703",
+    "url": "/m2/static/js/runtime-main.0e2ade93.js"
+  }
+]);
